a913c3f0c87778f9b4b8a4acb32a6fb4e4a09ebb
Ahmet Caliskan <noobitos@gmail.com>
2021-07-13 12:24:07 +0300

    added applayout

--- src/App.js
+++ src/App.js
@@ -1,9 +1,37 @@
+import React, { Component } from 'react';
 import './App.css';
+import Welcome from './Components/WelcomeMessage'
+import styled, {css} from 'styled-components'
+import AppLayout from './AppLayout'
 
-function App() {
+/*
+const MyButton = styled.div`
+color: green;
+${props => props.primary && css`
+    color: palevioletred;
+  `}
+`
+const TomatoButton = styled(MyButton)`
+  color: tomato;
+  border-color: tomato;
+`; */
+
+/* function App() {
   return (
 <div>Hello World !</div>
   );
+} */
+
+
+
+class App extends Component {
+  render() {
+    return (
+      <AppLayout>
+      <Welcome name={'Appazen'}/>
+      </AppLayout>
+    );
+  }
 }
 
 export default App;
